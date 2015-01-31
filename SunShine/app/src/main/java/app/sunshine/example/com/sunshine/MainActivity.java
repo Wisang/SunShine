61ad13d79eb845555dc96a13c388871c29ddba65
WisangsMac <wseom7@gmail.com>
2015-01-31 23:03:05 +0900

    list with fake data
    list view appears

--- SunShine/app/src/main/java/app/sunshine/example/com/sunshine/MainActivity.java
+++ SunShine/app/src/main/java/app/sunshine/example/com/sunshine/MainActivity.java
@@ -10,11 +10,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    // test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +58,7 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        static ArrayAdapter<String> mForecastAdapter;
 
         public PlaceholderFragment() {
         }
@@ -62,6 +67,31 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = {
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/40",
+                    "Tomorrow - Foggy - 70/40",
+                    "Tomorrow - Foggy - 70/40",
+                    "Tomorrow - Foggy - 70/40",
+                    "Tomorrow - Foggy - 70/40",
+                    "Tomorrow - Foggy - 70/40"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(
+                    Arrays.asList(forecastArray));
+
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast
+            );
+
+            ListView listView = (ListView)rootView.findViewById(
+                    R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
         }
     }
